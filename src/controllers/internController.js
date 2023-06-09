26620f3062aa26a9e3e7a15a4488b6fe33b22051
Saurabh <saurabhsahofficial@gmail.com>
2023-06-09 20:19:54 +0530

    update internController

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,24 +1,31 @@
+import eValidator from 'email-validator'
 import collegeModel from "../models/collegeModel.js"
 import internModel from "../models/internModel.js"
-import { isValid } from "../util/validator.js"
+import { isValid, isValidMN } from "../util/validator.js"
 
 export const createIntern = async (req, res) => {
     try {
-        
+
         if (Object.keys(req.body).length === 0) return res.status(400).json({ status: false, message: "Please, Provide details" })
         let { name, email, mobile, collegeName } = req.body
+
         if (!isValid(name)) return res.status(400).json({ status: false, message: "Please, Provide Name" })
+
         if (!isValid(email)) return res.status(400).json({ status: false, message: "Please, Provide Email" })
-        
-        const DbEmail = await internModel.findOne({email: email})
-        if (!DbEmail) return res.status(400).json({ status: false, message: "Email is already exist" })
+        if (!eValidator.validate(email)) return res.status(400).json({ status: false, message: "Invalid Email" })
+        const DbEmail = await internModel.findOne({ email: email })
+        if (DbEmail) return res.status(400).json({ status: false, message: "Email is already exist" })
 
         if (!isValid(mobile)) return res.status(400).json({ status: false, message: "Please, Provide Mobile No." })
+        if (!isValidMN(mobile)) return res.status(400).json({ status: false, message: "Invalid Mobile Number" })
+        const DbMobile = await internModel.findOne({ mobile: mobile })
+        if (DbMobile) return res.status(400).json({ status: false, message: "Mobile Number is already exist" })
+
         if (!isValid(collegeName)) return res.status(400).json({ status: false, message: "Please, Provide College Name" })
 
         const dbCollegeId = await collegeModel.findOne({ name: collegeName }).select({ _id: 1 })
         if (!dbCollegeId) return res.status(404).json({ status: false, message: "College not found" })
-        
+
         req.body.collegeId = dbCollegeId._id
 
         const saveData = await internModel.create(req.body)
